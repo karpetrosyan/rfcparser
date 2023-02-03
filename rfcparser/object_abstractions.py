aea59271be5b4c7d1f6f0a08d2dd6ebffa791256
karosis88 <kar.petrosyanpy@gmail.com>
2023-02-03 18:23:48 +0400

    Change SetCookie6265 __init__ method, add update_relative_ref and default_path methods

--- rfcparser/object_abstractions.py
+++ rfcparser/object_abstractions.py
@@ -1,8 +1,56 @@
+from datetime import datetime
+
+
+def default_path(uri):
+    uri_path = uri.path
+
+    if uri_path is None:
+        return "/"
+    if uri_path.count("/") == 1:
+        return "/"
+    return ''.join(uri_path.split('/')[:-1])
+
+
 class SetCookie6265:
-    def __init__(self, key, value, attrs):
+    def __init__(self,
+                 key,
+                 value,
+                 uri,
+                 attrs):
         self.key = key
         self.value = value
-        self.attrs = attrs
+        self.creation_time = self.last_access_time = datetime.now()
+        self.persistent_flag = False
+        self.expiry_time = None
+        self.domain = attrs.get('Domain', "")
+
+        max_age = attrs.get('Max-Age', None)
+        if max_age:
+            self.persistent_flag = True
+            self.expiry_time = max_age
+        else:
+            expires = attrs.get('Expires', None)
+            if expires:
+                self.persistent_flag = True
+                self.expiry_time = expires
+
+        path = attrs.get("Path", None)
+        if path:
+            self.path = path
+        else:
+            self.path = default_path(uri)
+        secure = attrs.get('Secure', False)
+        self.secure_only_flag = secure
+        httponly = attrs.get("HttpOnly", False)
+        self.http_only_flag = httponly
+
+    def __str__(self):
+        return f"{self.key}={self.value}"
+
+    def __repr__(self):
+        return f"<SetCookie6265 {str(self)} {self.creation_time=} {self.expiry_time=} {self.path=} " \
+               f"{self.persistent_flag=} {self.http_only_flag=} {self.domain=} {self.last_access_time=}>" \
+               f"{self.secure_only_flag=}"
 
 
 class Uri3986:
@@ -15,6 +63,16 @@ class Uri3986:
         self._path = path
         self.query = query
         self.fragment = fragment
+
+    def updated_relative_ref(self, value):
+        hostname = self.ip or ".".join(self.host)
+        port = f":{self.port}" if self.port else ""
+        userinfo = f"{self.userinfo}@" if self.userinfo else ""
+
+        if value.startswith('//'):
+            return f"{self.scheme}:{value}"
+        else:
+            return f"{self.scheme}://{userinfo}{hostname}{port}{value}"
 
     @property
     def path(self):
